b1c330a35ed58f7b6b6d62200c102c7dfa815a05
Suellen <73621600+suellen05@users.noreply.github.com>
2022-06-22 22:22:27 -0300

    Agenda salvamento

--- agenda-salvamento/www/js/app.js
+++ agenda-salvamento/www/js/app.js
@@ -1,15 +1,30 @@
-function SalvarContato(){
-    nome = document.getElementById("nome").value 
-    telefone = document.getElementById("telefone").value 
+function SalvarContato() {
+    nome = document.getElementById("nome").value
+    telefone = document.getElementById("telefone").value
 
- novoContato = nome + " - " + telefone
- document.getElementById("lista-Contato").innerHTML = novoContato
+    novoContato = nome + " - " + telefone
+    document.getElementById("lista-Contato").innerHTML = novoContato
 
+    localStorage.listaContatos = novoContato
 
 }
 
-btSalvar = document.getElementById ("salvar")
-btSalvar = addEventListener ("click",function(){
+btSalvar = document.getElementById("salvar")
+btSalvar.addEventListener("click", function () {
     SalvarContato()
 
+})
+
+if (localStorage.listaContatos)
+    document.getElementById("lista-Contato").innerHTML = localStorage.listaContatos
+
+
+    function limpando() {
+        localStorage.clear();
+    }
+
+    
+btLimpar = document.getElementById("limpar")
+btLimpar.addEventListener("click", function () {
+    limpando()
 })
